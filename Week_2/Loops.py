ed7452d58f02ed55fedc1eebf87651f00bc34560
AwendyG <awendygrows@gmail.com>
2025-08-13 00:05:07 +0300

    practise

--- Week_2/Loops.py
+++ Week_2/Loops.py
@@ -7,12 +7,34 @@
 #else:
   #print("i is greater than or equal to 5")   
 
-number = 7
-guess = None
+#number = 7
+#guess = None
 
-while guess != number:
+#while guess != number:
 
-    guess = input("Guess again: ")
-    guess = int(guess)
+    #guess = input("Guess again: ")
+    #guess = int(guess)
 
-print("You did it!")
+#print("You did it!")
+
+#animal = "Elephant"
+#guess = None
+
+#while guess != animal:
+    
+  #guess = input( "Try once more: ")
+
+#print("You are lovely!")  
+
+amount = 1000
+amount = int(amount)
+guess = 0
+guess =int(guess)
+
+while guess < amount:
+
+  add_more = int(input("Add more:"))
+
+  guess=guess+add_more
+
+print("Yes, that's it")  
